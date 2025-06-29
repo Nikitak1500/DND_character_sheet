21f80d864097aa25d6d23bb0907e005d4e155c2b
Микита Кондратьєв <123njkz@gmail.com>
2025-06-29 17:11:37 +0300

    Initial commit

---
+++ app/src/main/java/com/nikitak/dndcharactersheet/model/Character.kt
@@ -0,0 +1,13 @@
+package com.nikitak.dndcharactersheet.model
+
+class Character(
+    var name: String,
+    var dnd_class: String,
+    var character_lvl: Int,
+    var race: String,
+    var background: String,
+    var alignment: String,
+    var exp: Int,
+    var player: String,
+) {
+}
