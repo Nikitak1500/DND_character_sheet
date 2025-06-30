8146d0d8736e4c6d2a99a68ac968d800aece2aa3
Микита Кондратьєв <123njkz@gmail.com>
2025-06-30 22:05:19 +0300

    CHA-1 Add Characteristics

---
+++ app/src/main/java/com/nikitak/dndcharactersheet/model/Characteristics.kt
@@ -0,0 +1,12 @@
+package com.nikitak.dndcharactersheet.model
+
+class Characteristics(
+    var strength: Int = 10,
+    var dexterity: Int = 10,
+    var constitution: Int = 10,
+    var intelligence: Int = 10,
+    var wisdom: Int = 10,
+    var charisma: Int = 10
+) {
+
+}
