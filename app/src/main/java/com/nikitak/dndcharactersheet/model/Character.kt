64fa16f0ac60277b49b45e65d6e11b80574d49a5
Микита Кондратьєв <123njkz@gmail.com>
2025-06-30 22:05:19 +0300

    CHA-1 Add Characteristics

--- app/src/main/java/com/nikitak/dndcharactersheet/model/Character.kt
+++ app/src/main/java/com/nikitak/dndcharactersheet/model/Character.kt
@@ -1,13 +1,15 @@
 package com.nikitak.dndcharactersheet.model
 
 class Character(
+    var id: Int,
     var name: String,
-    var dnd_class: String,
-    var character_lvl: Int,
-    var race: String,
-    var background: String,
-    var alignment: String,
-    var exp: Int,
+    var dndClass: String?,
+    var characterLvl: Int = 1,
+    var species: String?,
+    var background: String?,
+    var alignment: String?,
+    var exp: Int = 0,
     var player: String,
+    var baseCharacteristics: Characteristics?,
 ) {
 }
